1c0ec142e243a72bcbf744693be324e5e8deb4bb
Dmytro Momot <momotdev@gmail.com>
2021-10-20 00:49:54 +0300

    add basic markup multiselect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import MultiSelect from "./components/MultiSelect/MultiSelect";
+import './styles/app.css';
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	const options = [
+		{value: 'ADMIN', label: 'Admin'},
+		{value: 'USER', label: 'User'},
+		{value: 'MANAGER', label: 'Manager'},
+		{value: 'DELIVERY', label: 'Delivery'},
+	]
+
+	return (
+		<div className="app">
+			<div className="content">
+				<MultiSelect options={options}/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
